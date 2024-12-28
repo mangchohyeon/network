fed3f90064311e4104dfd00c66105831c21f36c2
mangchohyeon <qgeon0419@gmail.com>
2024-12-28 20:27:38 +0900

    Create Formula.h

---
+++ Formula.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class formula
+{
+private :
+	string frml;
+public :
+	formula();
+	void set_frml(string frm);
+	void convert(void);
+};
